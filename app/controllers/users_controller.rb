9d1dc0c236082f50c629e447265e5eb3f5634ff5
EC2 Default User <ec2-user@ip-172-31-37-22.ec2.internal>
2022-12-03 14:20:26 +0000

    Final Commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @book = @user.books
+    @newbook = Book.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    if current_user != @user
+      redirect_to books_path
+    else
+      render :edit
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Profile was successfully updated."
+      redirect_to user_path
+    else
+      render :edit
+    end
+  end
+
+  def index
+    @users = User.all
+    @newbook = Book.new
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+
+end
