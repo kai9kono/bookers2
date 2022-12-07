67b0fd46ae6d44d7f7695e6158cbce0af1f9a5ed
EC2 Default User <ec2-user@ip-172-31-37-22.ec2.internal>
2022-12-07 05:56:20 +0000

    課題4

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
 
   validates :name, length: {minimum: 2, maximum: 20}, uniqueness: {:message => "Username already exisits. Please select a different one."}
   validates :introduction, length: {maximum: 50}
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'book_comments/create'
+  get 'book_comments/destroy'
+  devise_for :users
   root to: "homes#top"
   get "home/about" => "homes#about", as: 'about'
-  resources :books, only: [:index, :show, :edit, :create, :destroy, :update]
-  devise_for :users
+  resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
   resources :users, only: [:show, :edit, :index, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
